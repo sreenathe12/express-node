ac0a1bdfb64835d9376054987d1ebbd70e684acb
user <chandra.sreenath@molex.com>
2018-01-22 13:58:27 -0600

    Master Express-node

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/TranscendController',function(req,res){
-
-  res.render('monitor');
+router.post('/TranscendController',function(req,res){
+    res.render('monitor');
 });
 
 module.exports = router;
